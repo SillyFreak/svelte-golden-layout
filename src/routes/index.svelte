a23d3aa4f88932f491074b5569cf50ce1998fd62
Clemens Koza <clemens.koza@gmx.at>
2021-10-09 13:22:28 +0200

    prepare for a more complete demo

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -30,23 +30,34 @@
 </script>
 
 <main>
-	<p>
-		<input type="checkbox" bind:checked={display} /> display
-		<input type="number" min="0" max="10" bind:value={columns} /> columns
-	</p>
-	<div class="layout-container">
-		{#if display}
-			<GoldenLayout config={layout} let:componentType let:componentState>
-				<svelte:component this={components[componentType]} {...componentState} />
-			</GoldenLayout>
-		{/if}
+	<div>
+		<h1>Config</h1>
+		<p>
+			<input type="checkbox" bind:checked={display} /> display
+			<input type="number" min="0" max="10" bind:value={columns} /> columns
+		</p>
+	</div>
+	<div>
+		<h1>Demo</h1>
+		<div class="layout-container">
+			{#if display}
+				<GoldenLayout config={layout} let:componentType let:componentState>
+					<svelte:component this={components[componentType]} {...componentState} />
+				</GoldenLayout>
+			{/if}
+		</div>
 	</div>
 </main>
 
 <style>
 	main {
-		width: 800px;
-		margin: 150px auto;
+		display: flex;
+		flex-direction: row;
+	}
+
+	main > div {
+		width: 50%;
+		padding: 2rem;
 	}
 
 	input[type='number'] {
@@ -54,7 +65,6 @@
 	}
 
 	.layout-container {
-		width: 800px;
 		height: 600px;
 
 		border: 1px solid black;
